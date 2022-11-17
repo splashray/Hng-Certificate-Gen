4c1edd0313282fafb030fc3df0285c65eb57f50c
Iyobosa Omoruyi <omosiyobo@gmail.com>
2022-11-17 12:15:31 +0100

    contac models added

---
+++ models/contactModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+
+const UserSchema = new mongoose.Schema({
+  firstName:{
+    type:String,
+    required:true
+  },
+  lastName:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique: true
+  },
+  phoneNumber:{
+    type:String,
+    required:true,
+    unique: true
+  }
+})
+
+module.exports = mongoose.model('User', UserSchema)
