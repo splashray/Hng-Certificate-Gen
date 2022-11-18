dfc31ccd10d144586cc7101defbe54b2ce6b9d73
feeleep <aliup45@gmail.com>
2022-11-18 12:42:51 +0100

    Database connection

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 /* eslint-disable linebreak-style */
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const config = require('./utils/config');
-const auth = require('./routes/authRouter');
-const users = require('./routes/userRouter');
-const notFound = require('./middlewares/not-found');
-const teamRoute = require('./routes/teamRouter');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const config = require("./utils/config");
+const auth = require("./routes/authRouter");
+const users = require("./routes/userRouter");
+const notFound = require("./middlewares/not-found");
+const teamRoute = require("./routes/teamRouter");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = express();
 
-mongoose.set('useCreateIndex', true);
-mongoose.connect(config.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-})
+const DB = process.env.DATABASE;
+mongoose.set("useCreateIndex", true);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log("Connection successful");
+  });
+/*mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(config.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => {
-    console.log('Connected to mongodb');
+    console.log("Connected to mongodb");
     // start server after database is connected
     app.listen(config.PORT, () => {
       console.log(`connected to backend - ${config.PORT}`);
@@ -26,25 +40,25 @@ mongoose.connect(config.MONGODB_URL, {
   })
   .catch((error) => {
     console.log(error.reason);
-  });
+  });*/
 
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to HNG-Certificate Api');
+app.get("/", (req, res) => {
+  res.send("Welcome to HNG-Certificate Api");
 });
 
 // routes
-app.use('/api/auth', auth);
-app.use('/api/users', users);
-app.use('/api/team', teamRoute);
+app.use("/api/auth", auth);
+app.use("/api/users", users);
+app.use("/api/team", teamRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
-  const errorMessage = err.message || 'Something went wrong!';
+  const errorMessage = err.message || "Something went wrong!";
   return res.status(errorStatus).json({
     success: false,
     status: errorStatus,
--- models/Member.js
+++ models/Member.js
@@ -1,33 +1,38 @@
 /* eslint-disable linebreak-style */
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const memberSchema = new Schema({
-  name: {
-    required: true,
-    type: String,
+const memberSchema = new Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      default: "member",
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    stack: {
+      required: true,
+      type: String,
+      enum: ["backend", "frontend", "marketing", "design", "product manager"],
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  role: {
-    type: String,
-    default: 'member',
-  },
-  avatar: {
-    type: String,
-    default: '',
-  },
-  stack: {
-    required: true,
-    type: String,
-    enum: ['backend', 'frontend', 'marketing', 'design', 'product manager'],
-  },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Member = model('Member', memberSchema);
+const Member = model("Member", memberSchema);
 
 module.exports = Member;
+
+
