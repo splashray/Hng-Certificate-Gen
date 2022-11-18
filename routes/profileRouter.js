17ee7770fb10cc61166c75af4c1bfa7f97582cac
Silver <Silverg33k@gmail.com>
2022-11-18 15:28:11 +0100

    feat(BE-7): added endpoint for deleting a certificate

---
+++ routes/profileRouter.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const router = express.Router()
+const Profile = require('../models/userProfile');
+const Certificate = require('../models/certificateModel');
+
+
+
+//This is for getting each user profile
+router.get("/:id", async (req, res) => {
+    const profile = await Profile.findById(req.params.id);
+  
+    if (!profile) return res.status(404).send('profile not available.');
+  
+    res.status(200).send(profile);
+  });
+
+
+
+  //This is for creating each user profile
+  router.post('/:id', async (req, res) => {
+   const  profile = new Profile({ 
+    avatar:req.body.avatar,
+    name: req.body.name,
+    job: req.body.job,
+    location: req.body.location,
+    email: req.body.email,
+    phoneNumber: req.body.phonenNmber,
+    });
+   await profile.save();
+   if(!profile)
+    return res.status(404).send('profile cannot be created')
+    
+    res.send(profile);
+  });
+
+
+  //this for updating profile
+  router.put('/:id', async (req, res) => {
+    if(!mongoose.isValidObjectId(req.params.id)){
+        res.status(400).send('invalid profile Id')
+    }
+    const profile = await Profile.findByIdAndUpdate(req.params.id,
+      { 
+      avatar:req.body.avatar,
+      name: req.body.name,
+      job: req.body.job,
+      location: req.body.location,
+      email: req.body.email,
+      phoneNumber: req.body.phonenNmber,
+      }, 
+      { new: true });
+  
+    if (!profile) return res.status(404).send('profile not updated.');
+    
+    res.send(profile);
+  });
+
+
+  router.delete('/:id', async (req, res) => {
+    const profile = await Profile.findByIdAndRemove(req.params.id);
+  
+    if (!profile) return res.status(404).send('The profile with the given ID was not found.');
+  
+    res.send("profile deleted");
+  });
+
+  router.delete('/certificate/:id', async (req, res) => {
+    const cert = await Certificate.findByIdAndRemove(req.params.id);
+  
+    if (!cert) return res.status(400).json({
+      message: 'Unable to delete certificate/certificate not found'
+    });
+  
+    res.json({
+      message: 'Certificate deleted'
+    });
+  });
+
+module.exports = router
