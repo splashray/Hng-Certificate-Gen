bbbaf725733a5add1aba0fd1f28264bc2741637f
Christian Ezeanyaeche <christianezeanyaeche@gmail.com>
2022-11-19 11:36:07 +0100

    feat: added subscription type to user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
 
-<<<<<<< HEAD
-const UserSchema = new Schema({
-  username: {
+const UserSchema = new mongoose.Schema({
+  name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
@@ -20,52 +18,24 @@ const UserSchema = new Schema({
     type: Boolean,
     default: false
   },
-  collections : [ {type : mongoose.Schema.Types.ObjectId, ref : 'Data'}] 
-=======
-
-const UserSchema = new mongoose.Schema({
-  google: {
-    id: {
-      type: String,
-    },
-    name: {
-      type: String,
-    },
-    email: {
-      type: String,
-    },
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-    email:{
-       type:String,
-       required:true,
-        unique: true
-    },
-    password:{
-      type:String,
-      required:true,
-    },
-    isAdmin:{
-       type: Boolean,
-       default: false
-  },
   avatar: {
     type: String,
-    },
+  },
   subscribed: {
     type: Boolean,
     required: true,
-    default: false
+    default: false,
+  },
+  subscription: {
+    type: String,
+    enum: ['basic', 'standard', 'premium']
   },
   trialAvailable: {
     type: Boolean,
     required: true,
     default: true
   },
->>>>>>> 979016fa2c51fd6765e1ad93e444ac58297d1837
+
 })
 
 module.exports = mongoose.model('User', UserSchema)
