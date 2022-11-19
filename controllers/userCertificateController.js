394d3ddb71a091e73f4df5714f94d2c4e9942360
Fadlullah <fadlulanre@gmail.com>
2022-11-19 13:34:34 +0100

    feat(BE-11): added number of certificate by user

--- controllers/userCertificateController.js
+++ controllers/userCertificateController.js
@@ -52,7 +52,22 @@ const getAllCertificates = async (req, res) => {
   res.status(200).json(certificates);
 };
 
+const getNumberOfCertificates = async (req, res) => {
+  const auth = req.headers.authorization;
+  if (!auth) {
+    return res.status(403).json({ error: "No credentials sent!" });
+  }
+
+  const token = auth.split(" ")[1];
+  const { userId } = jwt.decode(token);
+  const user = await User.findOne({ userId }).exec();
+  const cert = user.records;
+
+  res.status(200).json({result: cert, count: cert.length});
+};
+
 module.exports = {
   getAllCertificates,
   addCertificate,
+  getNumberOfCertificates
 };
--- routes/certificateRoute.js
+++ routes/certificateRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllCertificates, addCertificate } = require('../controllers/userCertificateController');
+const { getAllCertificates, addCertificate, getNumberOfCertificates } = require('../controllers/userCertificateController');
 
 router.get('/', getAllCertificates);
+router.get('/count', getNumberOfCertificates)
 router.post('/', addCertificate);
 
 module.exports = router;
