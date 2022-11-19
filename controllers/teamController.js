46f0234f19e18b66efab6f986f3baf37488afd29
feeleep <aliup45@gmail.com>
2022-11-19 14:33:31 +0100

    Updating the our team page endpoint

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -7,38 +7,34 @@ const Member = require("../models/Member");
 // get list of all team members with optional stack query
 exports.getTeamMembers = async (req, res, next) => {
   try {
-    const members = Member.find();
-
-    res.status(200).json({
-      members,
-    });
-    //   const { stack } = req.query;
-    //   if (stack) {
-    //     Member.find({ stack }, "role avatar email name", (err, team) => {
-    //       if (err) {
-    //         next(err);
-    //       }
-    //       if (team.length === 0) {
-    //         return res.status(404).json({
-    //           success: false,
-    //           message: "not found",
-    //         });
-    //       }
-    //       res.status(200).json(team);
-    //     });
-    //   } else {
-    //     Member.find({}, "role avatar team email name", (err, team) => {
-    //       if (err) {
-    //         next(err);
-    //       }
-    //       res.status(200).json(team);
-    //     });
-    //   }
+    const { stack } = req.query;
+    if (stack) {
+      Member.find({ stack }, "role avatar email name", (err, team) => {
+        if (err) {
+          next(err);
+        }
+        if (team.length === 0) {
+          return res.status(404).json({
+            success: false,
+            message: "not found",
+          });
+        }
+        res.status(200).json(team);
+      });
+    } else {
+      Member.find({}, "role avatar team email name", (err, team) => {
+        if (err) {
+          next(err);
+        }
+        res.status(200).json(team);
+      });
+    }
   } catch (error) {
     res.status(400).json({
       success: false,
       message: "Invalid request",
     });
+    console.log(error);
   }
   //   next(error);
   // }
@@ -63,42 +59,27 @@ exports.getTeamMember = async (req, res, next) => {
 
 exports.addTeamMember = async (req, res, next) => {
   try {
-    const member = await Member.create(req.body);
-    res.status(201).json({
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { body } = req;
+    const member = await new Member(body).save();
+    if (!member) {
+      return res.status(400).json({
+        success: false,
+        message: "bad request",
+      });
+    }
+    return res.status(200).json({
       success: true,
-      message: "Member added successfully",
+      message: "member added successfully",
+      id: member._id,
     });
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-      message: "bad request",
-    });
+  } catch (error) {
+    next(error);
   }
 };
-// try {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-//   const {
-//     body,
-//   } = req;
-//   const member = await new Member(body).save();
-//   if (!member) {
-//     return res.status(400).json({
-//       success: false,
-//       message: 'bad request',
-//     });
-//   }
-//   return res.status(200).json({
-//     success: true,
-//     message: 'member added successfully',
-//     id: member._id,
-//   });
-// } catch (error) {
-//   next(error);
-// }
-//};
 
 exports.updateTeamMember = async (req, res, next) => {
   try {
