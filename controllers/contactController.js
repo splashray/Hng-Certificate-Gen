8e5131aa0fc5e19644a24234b9ebffaf32ccc926
Iyobosa Omoruyi <omosiyobo@gmail.com>
2022-11-19 14:49:55 +0100

    fix/(BE-17): email send from auth_email

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -32,52 +32,11 @@ const sendContact =  async (req, res) => {
 
     newContact.save();
 
-  //   const mail1 = `
-  //   <p>You have a new contact request</p>
-  //   <h3>Contact Details</h3>
-  //   <ul>  
-  //     <li>Name: ${req.body.firstName} ${req.body.lastName}</li>
-  //     <li>Email: ${req.body.email}</li>
-  //     <li>Phone: ${req.body.phoneNumber}</li>
-  //   </ul>
-  //   <h3>Message</h3>
-  //   <p>${req.body.message}</p>
-  // `
-  //   // Create Transporter
-  //   let transporter1 = nodemailer.createTransport({
-  //     service: 'gmail',
-  //     auth: {
-  //       user: config.AUTH_EMAIL,
-  //       pass: config.AUTH_PASS,
-  //       // clientId: GOOGLE_CLIENT_ID,
-  //       // clientSecret: GOOGLE_CLIENT_SECRET
-  //       // refreshToken: GOOGLE_REFRESH_TOKEN
-  //     }
-  //   });
-
-  //   // Mail Options To Website Owner
-  //   let mailOptions1 = {
-  //     from: 'omosiyobo@gmail.com', //Sender address
-  //     to: 'omosiyobo@gmail.com', // Receiver address
-  //     subject: 'Hi From Certawi',
-  //     text: 'Hi from Certawi',
-  //     html: mail1
-  //   };
-
-  //   transporter1.sendMail(mailOptions1, function(err, data) {
-  //     if (err) {
-  //       console.log("Error: " + err);
-  //     } else {
-  //       console.log("Email sent successfully");
-  //       console.log(data);
-  //     }
-  //   });
-
-    const mail2 = `
+    const mail = `
     <p>Your message has been received.</p>
   `
     // Create Transporter
-    let transporter2 = nodemailer.createTransport({
+    let transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         user: config.AUTH_EMAIL,
@@ -89,15 +48,15 @@ const sendContact =  async (req, res) => {
     });
 
     // Mail Options To Website User
-    let mailOptions2 = {
-      from: 'omosiyobo@gmail.com', //Sender address
+    let mailOptions = {
+      from: `${config.AUTH_EMAIL}`, //Sender address
       to: `${req.body.email}`, // Receiver address
       subject: 'Hi From Certawi',
       text: 'Hi from Certawi',
-      html: mail2
+      html: mail
     };
 
-    transporter2.sendMail(mailOptions2, function(err, data) {
+    transporter.sendMail(mailOptions, function(err, data) {
       if (err) {
         console.log("Error: " + err);
       } else {
